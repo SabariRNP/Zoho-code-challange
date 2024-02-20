f5060a70e25e759f8b935dfc5667311b0583a8f1
Sabarinathan <sabarinathan@gigayasa.com>
2024-02-20 12:22:50 +0530

    Updated code

--- Print_sheets.py
+++ Print_sheets.py
@@ -1,99 +1,58 @@
-price=0;
-order=0;
-breadth=[210,297,420,594];       #constants
-length=[297,420,594,841];
-f=1;
-diff=[0,0,0,0];
-global i,n;                
-def findsheet(b,l):                     #best paper for given length,breadth is found here
-     global price,order,preorder,ch,prematch;ch=-1;
-     for i in range(4):
-          if (breadth[i]-b)<0 and(length[i]-l)<0:                  #difference between given breadth and alloted paper breadth(for all 4 paper) is calculated.
-               diff[i]=-(abs(breadth[i]-b)*abs(length[i]-l));      #similarly difference is calculated for length.then two diff is multiplied. 
-          else:
-               diff[i]=(breadth[i]-b)*(length[i]-l);  
-     thisdict ={diff[0]:"a4",diff[1]:"a3",diff[2]:"a2",diff[3]:"a1"};     #above calculated values are stored in dictionary and sorted
-     #print("unsorted dict",thisdict);
-     sortedlist=sorted(thisdict);#print("sorted dict",sortedlist);   
-     minpre=500000;
-     for i in range(4):                                  #least positive value(key in dictionary) in the dictionary is founded
-          mincur=sortedlist[i];                          #then corresponding value in the dictionary is required paper.  
-          if(sortedlist[i]>0 and mincur<minpre): 
-               minpre=mincur;
-               prematch=thisdict.get(sortedlist[i]);ch=1;#print("prematch=",prematch);
-     #print("findsheet ch=",ch);
-                    
-def checkmatch():
-     global j,totorder;
-     if(ch!=-1):                                #if best paper is founded, j value is incremented and
-         if(len(inputb)>1 and totorder!=0):     #again best paper match for incremented length and breadth is calculated 
-             j+=1;                              #until when match occur
-         return 1;
-     else:
-         if(j==0):
-             print("Order cannot be acommodated!!");exit(0);
-         return -1;
-prematch='a'         
-n=int(input("Enter no of orders:\t"));             #no of orders is stores in variable n
-totorder=n;
-if(n<1 or n>10000):                                #here n value is checked, i.e, n is valid value or not
-     print("Enter valid number(1-10000)");f=0;   
-if(f==1):
-    inputs={0:1};inputb=[];inputl=[];listb=[];listl=[];
-    for i in range(n):
-         print("Enter length (in mm) for order  ",i+1);        #length and breadth for each order is get and stored in list
-         xin=int(input());
-         print("Enter breadth(in mm) for order  ",i+1);
-         yin=int(input());
-         if(min(xin,yin) in inputs):
-             listb.append(min(xin,yin));listl.append(max(xin,yin));    
-         else:
-             inputs[min(xin,yin)]=max(xin,yin);
-    inputs.pop(0);
-    for i in sorted(inputs):                               
-         inputb.append(i);inputl.append(inputs[i]);
-    for k in range(len(listb)):                               #length list and breadth list are sorted
-        for i in range(len(inputb)):
-             if len(inputb)==1:
-                  if(listb[k]>=inputb[i]):                 
-                       inputb.insert(i+1,listb[k]);inputl.insert(i+1,listl[k]);break;
-             else:
-                  if(listb[k]>=inputb[i] and listb[k]<=inputb[i+1]):
-                       inputb.insert(i+1,listb[k]);inputl.insert(i+1,listl[k]);break;
-    #print("IP after sort",inputb,"\n",inputl);
+# Print sheets
+# In a printing press, there are four sizes of printing paper used A4, A3, A2, A1. For every order,
+# the user will give their desired page size (length and breadth).
+# Write a program to get the N orders from user and print the best price to the customer
+# Price chart (All size in mm)
+# A4 - Rs 10 (210 x 297)
+# A3 - Rs 15 (297 x 420)
+# A2 - Rs 20 (420 x 594)
+# A1 - Rs 25 (594 x 841)
+# Input:
+# No of orders - 2
+# Order 1
+# Length - 200
+# Breadth - 250
+# Order 2
+# Length - 400
+# Breadth - 250
+# Output
+# Rs 25
+
+sheets = {
+    "A4" : {"length": 210, "breath" : 297, "price": 10},
+    "A3" : {"length": 297, "breath" : 420, "price": 15},
+    "A2" : {"length": 420, "breath" : 594, "price": 20},
+    "A1" : {"length": 594, "breath" : 841, "price": 25},
+}
+
+nOrders = int(input("Enter number of orders: "))
+
+orders = []
+prices = []
+
+for i in range(nOrders):
+    print("Order ", i+1)
+    len = int(input("Length: "))
+    breath = int(input("Breadth: "))
+    orders.append((len, breath))
     
-    for i in range(n): 
-        if(inputb[i]<50 or inputl[i]<50 or inputl[i]>841 or inputb[i]>594):            #all entered length and breadth values are checked
-            print("Paper size too small or too long!!\nOrder cannot be accomodated!!");exit(0);
-    #print("len(inputb)",len(inputb));
-    while(len(inputb)>0):
-         j=0;
-         c=1; 
-         while(c>0 and len(inputb)>=1 and totorder!=0):                 #initially first order(length & breadth) values are feeded to findsheet function
-               #print("Process start");                                  #if best paper match is found, length and breadth values are incremented(i.e, sum of two order)
-               findsheet(sum(inputb[0:j+1]),sum(inputl[0:j+1]));totorder-=1;   #while loop stopped when paper match is not occur
-               if(totorder>0): 
-                    c=checkmatch();
-               #print("c",c);print("totorder",totorder);print("j",j);
-         #print("inputb",inputb);
-         if(ch<0):
-             j-=1;totorder+=1;
-         match=prematch;                            #price is calculated for previously matched one
-         if(match=='a4'):                    
-               order+=1;print("Order",order,"A4 paper - size",sum(inputb[0:j+1]),"X",sum(inputl[0:j+1]),"price Rs.10");price+=10;
-         elif(match=='a3'):
-               order+=1;print("Order",order,"A3 paper - size",sum(inputb[0:j+1]),"X",sum(inputl[0:j+1]),"price Rs.15");price+=15;
-         elif(match=='a2'):
-               order+=1;print("Order",order,"A2 paper - size",sum(inputb[0:j+1]),"X",sum(inputl[0:j+1]),"price Rs.20");price+=20;
-         elif(match=='a1'):
-               order+=1;print("Order",order,"A1 paper - size",sum(inputb[0:j+1]),"X",sum(inputl[0:j+1]),"price Rs.25");price+=25;
-         else:
-               print("Unmatch");
-         #print("last j",j);
-         if(match=='a4' or match=='a3' or match=='a2' or match=='a1'):         #length , breadth for taken orders are removed from the list inputl and inputb
-               while(j>=0):                                                  #process will continue upto finishing all orders
-                    inputb.pop(j);inputl.pop(j);#print("popped");
-                    j-=1;           
-    print("Total price=\tRs.",price);        #total price for all order is printed
-
-
+print("Orders: ", orders)
+for order in orders:
+    sortedSizes = sorted(order)
+    print("sortedSizes:" , sortedSizes)
+    price = 0
+    for sheet in sheets.keys():
+        print("Sheet: ", sheet)
+        sheetDict = sheets[sheet]
+        if((sortedSizes[0] <= sheetDict["length"]) and (sortedSizes[1] <= sheetDict["breath"])):
+            price = sheets[sheet]["price"]
+            break
+    
+    prices.append(price)
+    
+print("Prices for orders : ", prices)
+if(prices.count(0) > 0):
+    print("Order cannot be accomodated")
+    
+print("Total Price: Rs.", sum(prices))
+    
--- The_lift.py
+++ The_lift.py
@@ -1,40 +1,56 @@
-lift_pos=['3','G','6','12','10','11'];       #Initial position of all 6 lifts
-print(lift_pos);
-while(1):
-    f=1;
-    n=str(input("Enter your Floor:\t"));     #Requested floor number is get from user
-    if n.isalpha():
-        if n!='G':                                      #Entered floor number is checked before going to the actual process
-           print("Enter correct floor number!!"); 
-           f=0;
-    elif int(n)>12 or int(n)<1:
-        print("Enter correct floor number!!");
-        f=0;
-    else:
-        f=1;
-    if(f):
-        diff_pre=12;
-        for x in range(6):           
-           if lift_pos[x]=='G':              #If entered input or list position is 'G' then it is converted to 0
-                lift_pos[x]=0;
-           if n=='G':
-                n=0;
-           diff_cur=abs(int(lift_pos[x])-int(n));      #difference between n and each lift position is found 
-           if(diff_cur<diff_pre):                      #nearest lift is founded by minimum difference
-                pos=x; diff_pre=diff_cur;
-           if lift_pos[x]==0:              # value zero is changed to original char 'G'
-                lift_pos[x]='G';  
-        if n==0:                    
-            n='G';
-        if(diff_pre==0):                                  
-            print("Lift is already in requested floor");
-        else:
-            print("Nearest lift number\t",pos+1);         #nearest lift is printed
-            lift_pos[pos]=n;
-    print("Current lift position:",lift_pos);
-    y=input("Are You want to call the lift again(Y/N):\t");
-    print(y);
-    if(y!='Y'):
-       break;
-    
+# The lift
+# A 12 story building has 6 lifts at a center. Lifts are scattered along multiple ﬂoors. Assume a
+# user calls lift from N th ﬂoor. Write a program to ﬁnd the nearest lift for pickup and track all
+# the lift movements and current location
+# Assume lifts are in this ﬂoors -> 3,G,6,12,10,11
+# For example
+# Input
+# Lift positions
+# 3,G,6,12,10,11
+# Enter your floor number: 7
+# Nearest lift number: 3 //Explanation: Lift
+# floor
+# Enter your floor number: 8
+# Nearest lift: 3 //Explanation: Lift
+# floor in above transition
+# number 3 is currently in 6th
+# number 3 is moved from 6th floor to 7th
 
+
+
+lift_pos=['3','G','6','12','10','11']
+print("Current Lift positions: ", lift_pos)
+ 
+while 1:
+    floor = str(input("Enter your floor: "))
+    if(floor == "G"):
+        floor = 0
+        
+    floor = int(floor)
+    
+    nearestLift = 0, 
+    diff = 13
+    for i, Lpos in enumerate(lift_pos):
+        cLPos = Lpos
+        if(cLPos == "G"):
+            cLPos = 0
+        cLPos = int(cLPos)
+        
+        if(abs(cLPos - floor)  < diff):
+            nearestLift = i+1
+            diff = abs(cLPos - floor)
+        
+    if(diff == 0):
+        print("Lift {} is already at the requested floor".format(nearestLift))
+    else:
+        print("Nearest list is : ", nearestLift)
+        
+    if(floor == 0):
+        floor = "G"
+        
+    lift_pos[nearestLift-1] = floor
+    print("Current Lift positions: ", lift_pos)
+    
+    stop  = str(input("Are You want to call the lift again(Y/N):\t"))
+    if(stop != "Y" and stop != "y"):
+        break
